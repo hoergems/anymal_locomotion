76f911932b92c195cd45b7f07fdef305243341b5
hoergems <hoergems@gmail.com>
2026-07-06 12:21:59 +1000

    Initial commit

---
+++ source/anymal_locomotion/anymal_locomotion/tasks/direct/anymal_locomotion/__init__.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import gymnasium as gym
+
+from . import agents
+
+##
+# Register Gym environments.
+##
+
+
+gym.register(
+    id="Template-Anymal-Locomotion-Direct-v0",
+    entry_point=f"{__name__}.anymal_locomotion_env:AnymalLocomotionEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.anymal_locomotion_env_cfg:AnymalLocomotionEnvCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+    },
+)
